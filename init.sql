2713c1dd6ce08efbc503dcd4e114092da1f25b63
EL HIRACH YASSINE <yassine.el-hirach@etu.univ-lyon1.fr>
2022-12-11 17:13:13 +0100

    script sql pour lancer l'application avec des données en plus, (ne marche pas encore avec docker)

---
+++ init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Product (id INT NOT NULL, type VARCHAR(100) NOT NULL, title VARCHAR(150) NOT NULL, price INT NOT NULL, PRIMARY KEY(id));
+
+INSERT INTO Product VALUE(1, "boisson", "Coca Cola", 2);
+INSERT INTO Product VALUE(2, "sandwicherie", "Panini", 3);
