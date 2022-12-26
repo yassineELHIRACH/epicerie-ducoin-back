9503ba52889145495501d0af7f192f3ff5b4cbfb
EL HIRACH YASSINE <yassine.el-hirach@etu.univ-lyon1.fr>
2022-12-26 15:27:53 +0100

    maj format

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,26 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BasketsModule } from './basket/baskets.module';
 import { ProductsModule } from './products/products.module';
 
-
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: 'root',
-            database: 'market',
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: true,
-        }),
-        TypeOrmModule.forFeature([
-            ProductsModule,
-            BasketsModule
-        ]),
-        ProductsModule,
-        BasketsModule],
-    controllers: [],
-    providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'market',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([ProductsModule, BasketsModule]),
+    ProductsModule,
+    BasketsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule { }
+export class AppModule {}
