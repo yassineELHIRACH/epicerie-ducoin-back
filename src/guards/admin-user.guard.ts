440289870bd881bd22ea7207bc0c99a73423ee53
EL HIRACH YASSINE <yassine.el-hirach@etu.univ-lyon1.fr>
2022-12-26 17:05:50 +0100

    Guards pour la POST, PUT et DELETE uniquement admin

---
+++ src/guards/admin-user.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+    canActivate(
+        context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+
+        const request = context.switchToHttp().getRequest();
+        const { headers } = request;
+
+        if (
+            !headers.authorization ||
+            !headers.authorization.includes('admin')
+        ) {
+            return false
+        }
+
+        return true;
+    }
+}
